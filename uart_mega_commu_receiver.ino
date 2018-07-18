71e51857702c5f299b39ff730ecb34c2ea7bdb56
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-07-18 15:53:29 +0900

    Update uart_mega_commu_receiver.ino

--- uart_mega_commu_receiver.ino
+++ uart_mega_commu_receiver.ino
@@ -1,12 +1,12 @@
 void setup(){
   Serial1.begin(9600);
-  Serial.begin(9600);
+
 }
 
 void loop(){
   while(Serial1.available()){
     char data = Serial1.read();
     Serial1.println(data);
-    Serial.println(data);
+
   }  
 }
