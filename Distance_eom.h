bb6b1eb8d2d8004b2e5cbc5f52f4d71ba67017cb
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-07-20 17:36:08 +0900

    Distance and bearing

--- Distance_eom.h
+++ Distance_eom.h
@@ -2,7 +2,8 @@
 #define TO_RAD (3.1415926536 / 180)
 #include <math.h>
 
-double dist(double th1, double ph1, double th2, double ph2)
+// getting distance 
+double get_dist(double th1, double ph1, double th2, double ph2)
 {
 	double dx, dy, dz;
 	ph1 -= ph2;
@@ -11,10 +12,12 @@ double dist(double th1, double ph1, double th2, double ph2)
 	dz = sin(th1) - sin(th2);
 	dx = cos(ph1) * cos(th1) - cos(th2);
 	dy = sin(ph1) * cos(th1);
+	
 	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
 }
 
-double bear(double th1, double ph1, double th2, double ph2) {
+//getting bearing
+double get_bear(double th1, double ph1, double th2, double ph2) {
 
 	double rlat1 = TO_RAD * th1;
 	double rlat2 = TO_RAD * th2;
@@ -26,6 +29,6 @@ double bear(double th1, double ph1, double th2, double ph2) {
 		sin(rlat1) * cos(rlat2) * cos(rlon2-rlon1);
 	double brng = atan2(y, x) * (180/3.1415926536);
 
-	return 360 - brng;
+	return brng;
 
 }
