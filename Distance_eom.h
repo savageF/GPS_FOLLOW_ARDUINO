414875300296a42b6d31c9e02b7e2d01b0f62a71
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-07-20 16:26:48 +0900

    get distance between becon and follower[km]

---
+++ Distance_eom.h
@@ -0,0 +1,13 @@
+#define R 6371
+#define TO_RAD (3.1415926536 / 180)
+double dist(double th1, double ph1, double th2, double ph2)
+{
+	double dx, dy, dz;
+	ph1 -= ph2;
+	ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+
+	dz = sin(th1) - sin(th2);
+	dx = cos(ph1) * cos(th1) - cos(th2);
+	dy = sin(ph1) * cos(th1);
+	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
