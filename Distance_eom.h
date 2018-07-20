f7f7412bcd06a606b33c3b428d5473ccaa091708
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-07-20 17:20:20 +0900

    Update Distance_eom.h

--- Distance_eom.h
+++ Distance_eom.h
@@ -1,5 +1,7 @@
 #define R 6371
 #define TO_RAD (3.1415926536 / 180)
+#include <math.h>
+
 double dist(double th1, double ph1, double th2, double ph2)
 {
 	double dx, dy, dz;
@@ -10,4 +12,20 @@ double dist(double th1, double ph1, double th2, double ph2)
 	dx = cos(ph1) * cos(th1) - cos(th2);
 	dy = sin(ph1) * cos(th1);
 	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
+
+double bear(double th1, double ph1, double th2, double ph2) {
+
+	double rlat1 = TO_RAD * th1;
+	double rlat2 = TO_RAD * th2;
+	double rlon1 = TO_RAD * ph1;
+	double rlon2 = TO_RAD * ph2;
+	
+	double y = sin(rlon2-rlon1) * cos(rlat2);
+	double x = cos(rlat1) * sin(rlat2) -
+		sin(rlat1) * cos(rlat2) * cos(rlon2-rlon1);
+	double brng = atan2(y, x) * (180/3.1415926536);
+
+	return 360 - brng;
+
 }
