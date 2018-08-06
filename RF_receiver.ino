ce6a9dfa95edaedf93d51c02087ed9a7870cb8d0
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-08-06 21:04:35 +0900

    Update RF_receiver.ino
    RF -> virturewire lib
    DISTANCE CALC -> DISTANCE_EOM lib
    GPS -> TinyGPS lib

--- RF_receiver.ino
+++ RF_receiver.ino
@@ -1,31 +1,58 @@
+//struct receive
+#include <Distance.h>
+#include <VirtualWire.h>
+#include <TinyGPS.h>
 
-// Include RadioHead Amplitude Shift Keying Library
-#include <RH_ASK.h>
-// Include dependant SPI Library 
-#include <SPI.h> 
-// D11 - DATA CONNECTING
-// Create Amplitude Shift Keying Object
-RH_ASK rf_driver;
+struct DATA{
+ float X;
+ float Y;
+};
+
+struct DATA *DataIn;
 
 void setup()
 {
-    // Initialize ASK Object
-    rf_driver.init();
-    // Setup Serial Monitor
-    Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.begin(115200);
+  vw_setup(2000);
+  vw_set_rx_pin(11);
+  vw_rx_start();
 }
+
+float flat , flon;
 
 void loop()
 {
-    // Set buffer to size of expected message
-    uint8_t buf[36];
-    uint8_t buflen = sizeof(buf);
-    // Check if received packet is correct size
-    if (rf_driver.recv(buf, &buflen))
-    {
-      
-      // Message received with valid checksum
-      Serial.print("Message Received: ");
-      Serial.println((char*)buf);         
-    }
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) 
+  {
+       if (Serial1.available())
+       {
+          validGPS = gps.encode(Serial1.read());
+       }
+       if (validGPS)
+       {
+          gps.f_get_position(&flat, &flon);
+       }
+    DataIn = (struct DATA*)buf;
+    Serial.println();
+    Serial.println("위도 와 경도");
+    Serial.println(DataIn->X);
+    Serial.println(DataIn->Y);
+
+    Serial.println();
+    Serial.println("대상 까지의 거리");
+    Serial.print(get_dist(flat,flon,(double)DataIn->X , (double)DataIn->Y) * 1000);
+    Serial.print(" Meter");
+
+    Serial.println();
+    Serial.println("대상에 대한 각");
+    Serial.print(get_bear(flat,flon,(double)DataIn->X , (double)DataIn->Y) - 360);
+    Serial.print(" deg");
+    Serial.println();
+   
+    delay(5000);
+  }
 }
