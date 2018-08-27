85b3ed0eca78950efabca93ef6791aa899c96908
savage_for_code <34770707+savageF@users.noreply.github.com>
2018-08-27 14:33:51 +0900

    Update RF_receiver.ino

--- RF_receiver.ino
+++ RF_receiver.ino
@@ -1,7 +1,34 @@
-//struct receive
+// Test code for Ultimate GPS Using Hardware Serial (e.g. GPS Flora or FeatherWing)
+//
+// This code shows how to listen to the GPS module via polling. Best used with
+// Feathers or Flora where you have hardware Serial and no interrupt
+//
+// Tested and works great with the Adafruit GPS FeatherWing
+// ------> https://www.adafruit.com/products/3133
+// or Flora GPS
+// ------> https://www.adafruit.com/products/1059
+// but also works with the shield, breakout
+// ------> https://www.adafruit.com/products/1272
+// ------> https://www.adafruit.com/products/746
+// 
+// Pick one up today at the Adafruit electronics shop
+// and help support open source hardware & software! -ada
+     
+#include <Adafruit_GPS.h>
 #include <Distance.h>
 #include <VirtualWire.h>
-#include <TinyGPS.h>
+
+// what's the name of the hardware serial port?
+#define GPSSerial Serial1
+
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+     
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
+
+uint32_t timer = millis();
 
 struct DATA{
  float X;
@@ -9,55 +36,95 @@ struct DATA{
 };
 
 struct DATA *DataIn;
-TinyGPS gps;
-boolean validGPS = false;
+
 void setup()
 {
-  Serial2.begin(9600);
-  Serial.begin(9600);
-  vw_setup(2000);
+  vw_setup(5000);
   vw_set_rx_pin(11);
   vw_rx_start();
+  //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
+  
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic test!");
+     
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+     
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+  
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
 }
 
-float flat , flon;
-
-void loop()
+void loop() // run over and over again
 {
+
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-  if (vw_get_message(buf, &buflen)) 
-  {
-       if (Serial2.available())
-       {
-          validGPS = gps.encode(Serial2.read());
-       }
-       if (validGPS)
-       {
-          gps.f_get_position(&flat, &flon);
-       }
-    DataIn = (struct DATA*)buf;
-    Serial.println();
-    Serial.println("위도 와 경도");
-    Serial.println(DataIn->X);
-    Serial.println(DataIn->Y);
-
-    Serial.println("위도 와 경도2");
-    Serial.println(flat);
-    Serial.println(flon);
-
-    Serial.println();
-    Serial.println("대상 까지의 거리");
-    Serial.print(get_dist((double)flat,(double)flon,(double)DataIn->X , (double)DataIn->Y) * 1000);
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis()) timer = millis();
+     
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix && vw_get_message(buf, &buflen) ) {
+      DataIn = (struct DATA*)buf;
+      Serial.println();
+      Serial.println("Flat and flon");
+      Serial.println(DataIn->X,6);
+      Serial.println(DataIn->Y,6);
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 6);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 6); 
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    Serial.println("distance from ~");
+    Serial.print(get_dist((double)GPS.latitude/100,(double)GPS.longitude/100,(double)DataIn->X , (double)DataIn->Y));
     Serial.print(" Meter");
-
-    Serial.println();
-    Serial.println("대상에 대한 각");
-    Serial.print(get_bear((double)flat,(double)flon,(double)DataIn->X , (double)DataIn->Y) - 360);
-    Serial.print(" deg");
-    Serial.println();
-   
-    delay(500);
+ Serial.println("bearing from ~");
+    Serial.print(get_bear((double)GPS.latitude/100,(double)GPS.longitude/100,(double)DataIn->X , (double)DataIn->Y)-360);
+    Serial.print(" Deg");
+    }
   }
 }
